ad06f7af4cfc792bcdcaed0b3d16edae467c2c97
zeromov <355316219@qq.com>
2017-09-20 17:15:16 +1000

    search for the middle number

--- quiz1.py
+++ quiz1.py
@@ -1,48 +1,36 @@
-# Written by *** and Eric Martin for COMP9021
+import math
 
-
-'''
-Generates a list L of random nonnegative integers smaller than the length of L,
-whose value is input by the user, and outputs two lists:
-- a list M consisting of L's middle element, followed by L's first element,
-  followed by L's last element, followed by L's second element, followed by
-  L's penultimate element...;
-- a list N consisting of L[0], possibly followed by L[L[0]], possibly followed by
-  L[L[L[0]]]..., for as long as L[L[0]], L[L[L[0]]]... are unused, and then,
-  for the least i such that L[i] is unused, followed by L[i], possibly followed by
-  L[L[i]], possibly followed by L[L[L[i]]]..., for as long as L[L[i]], L[L[L[i]]]...
-  are unused, and then, for the least j such that L[j] is unused, followed by L[j],
-  possibly followed by L[L[j]], possibly followed by L[L[L[j]]]..., for as long as
-  L[L[j]], L[L[L[j]]]... are unused... until all members of L have been used up.
-'''
-
-
-import sys
-from random import seed, randrange
-
-
-try:
-    arg_for_seed, length = input('Enter two nonnegative integers: ').split()
-except ValueError:
-    print('Incorrect input, giving up.')
-    sys.exit()
-try:
-    arg_for_seed, length = int(arg_for_seed), int(length)
-    if arg_for_seed < 0 or length < 0:
-        raise ValueError
-except ValueError:
-    print('Incorrect input, giving up.')
-    sys.exit()
-
-seed(arg_for_seed)
-L = [randrange(length) for _ in range(length)]
-print('\nThe generated list L is:')
-print('  ', L)
+L =[1,2,3,4,5]
+# Replace this comment with your code
 M = []
 N = []
 
-# Replace this comment with your code
-    
+
+
+
+def push_middle(L):
+    M_f =[]
+    Pop_index=0
+    Middle_index=math.floor(len(L)/2)
+    if len(L)==1:
+        return L
+    else:
+        M_f.append(L[Middle_index])
+        for index in range(Middle_index):
+            print(index)
+            M_f.append(L[index])
+            if len(L)%2 ==0:
+                M_f.append(L[-(index)+1])
+            else:
+                M_f.append(L[-(index+1)])
+        
+        print('列表的中值：',M_f[0])
+        print('列表的中值索引：',Middle_index)
+        
+        print(M_f)
+        
+M=push_middle(L)        
+
 print('\nHere is M:')
 print('  ', M)
 print('\nHere is N:')
