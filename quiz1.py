c6a605da50a91654f74d667b6a87043258aebcca
zeromov <355316219@qq.com>
2017-09-20 17:20:56 +1000

    accomplish the put the middle one to first

--- quiz1.py
+++ quiz1.py
@@ -1,7 +1,9 @@
+
+# Replace this comment with your code
 import math
 
-L =[1,2,3,4,5]
-# Replace this comment with your code
+L =[1,2,3]
+
 M = []
 N = []
 
@@ -10,24 +12,22 @@ N = []
 
 def push_middle(L):
     M_f =[]
-    Pop_index=0
     Middle_index=math.floor(len(L)/2)
     if len(L)==1:
         return L
     else:
         M_f.append(L[Middle_index])
-        for index in range(Middle_index):
-            print(index)
-            M_f.append(L[index])
-            if len(L)%2 ==0:
-                M_f.append(L[-(index)+1])
-            else:
+        if len(L)%2 ==0:
+            for index in range(Middle_index):
+                M_f.append(L[index])
                 M_f.append(L[-(index+1)])
-        
-        print('列表的中值：',M_f[0])
-        print('列表的中值索引：',Middle_index)
-        
-        print(M_f)
+            M_f.pop()
+        else:
+             for index in range(Middle_index):
+                M_f.append(L[index])
+                M_f.append(L[-(index+1)])
+    return M_f
+
         
 M=push_middle(L)        
 
