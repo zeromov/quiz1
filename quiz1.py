bf7dbabcbd31be7fb0395bc3a275b57c54059d26
zeromov <355316219@qq.com>
2017-09-20 17:32:16 +1000

    accomplish2

--- quiz1.py
+++ quiz1.py
@@ -1,71 +1,23 @@
 from collections import OrderedDict
-L =[3,4,1,6,7,2,1,1]
+L =[4,5,8,0,7,3,0,2,1]
 
-d = OrderedDict()
-for index in range(len(L)):
-    d[index]=L[index]
-    
-N_f=[]
-Check_exist=set()
-N_f.append(d[0])
-# if not 0
-#Check_exist.add(0)
+def Generate_order(L):
+        d = OrderedDict()
+        for index in range(len(L)):
+            d[index]=L[index]   
+        N_f=[]
+        Check_exist=set()
+        index = 0
+        while(len(Check_exist)!=len(L)):
+            if index not in Check_exist:     
+                N_f.append(d[index])
+                Check_exist.add(index)
+                index = d[index]
+            else:
+                for b in range(len(L)):
+                    if b not in Check_exist:
+                        index = b
+                        break
+        return N_f
+print(Generate_order(L))
 
-#for index in range(len(L)):
-    #print(index)
-index = 0
-#while(len(Check_exist)!=(len(L))):
-if d[index] not in Check_exist:
-    #N_f.append(d[index])
-    Check_exist.add(index)
-    index = d[index]
-if d[index] not in Check_exist:
-    N_f.append(d[index])
-    Check_exist.add(index)
-    index = d[index]
-if d[index] not in Check_exist:
-    N_f.append(d[index])
-    Check_exist.add(index)
-    index = d[index]
-if d[index] not in Check_exist:
-    N_f.append(d[index])
-    Check_exist.add(index)
-    index = d[index]
-if d[index] not in Check_exist:
-    N_f.append(d[index])
-    Check_exist.add(index)
-    index = d[index]
-    print(index)
-    if d[index] not in Check_exist:
-        N_f.append(d[index])
-        Check_exist.add(index)
-        index = d[index]
-        print(index)
-    else:
-        for i in range(len(L)):
-            if(i) not in Check_exist:
-                N_f.append(d[i])
-                Check_exist.add(i)
-                index = d[i]
-                print(index)
-                break
-        if d[index] not in Check_exist:
-            N_f.append(d[index])
-            Check_exist.add(index)
-            index = d[index]
-            print(index)
-        else:
-            for i in range(len(L)):
-                if(i) not in Check_exist:
-                    N_f.append(d[i])
-                    Check_exist.add(i)
-                    index = d[i]
-                    print(index)
-                    break    
-        
-  
-     
-  
-
-print(N_f)
-print(Check_exist)
