64142bc2dd49319699b5505ad57e806861cb8113
zeromov <355316219@qq.com>
2017-09-20 17:25:40 +1000

    use the loop try to get the logic

--- quiz1.py
+++ quiz1.py
@@ -1,39 +1,71 @@
+from collections import OrderedDict
+L =[3,4,1,6,7,2,1,1]
 
-# Replace this comment with your code
-import math
+d = OrderedDict()
+for index in range(len(L)):
+    d[index]=L[index]
+    
+N_f=[]
+Check_exist=set()
+N_f.append(d[0])
+# if not 0
+#Check_exist.add(0)
 
-L =[1,2,3]
-
-M = []
-N = []
-
-
-
-
-def push_middle(L):
-    M_f =[]
-    Middle_index=math.floor(len(L)/2)
-    if len(L)==1:
-        return L
+#for index in range(len(L)):
+    #print(index)
+index = 0
+#while(len(Check_exist)!=(len(L))):
+if d[index] not in Check_exist:
+    #N_f.append(d[index])
+    Check_exist.add(index)
+    index = d[index]
+if d[index] not in Check_exist:
+    N_f.append(d[index])
+    Check_exist.add(index)
+    index = d[index]
+if d[index] not in Check_exist:
+    N_f.append(d[index])
+    Check_exist.add(index)
+    index = d[index]
+if d[index] not in Check_exist:
+    N_f.append(d[index])
+    Check_exist.add(index)
+    index = d[index]
+if d[index] not in Check_exist:
+    N_f.append(d[index])
+    Check_exist.add(index)
+    index = d[index]
+    print(index)
+    if d[index] not in Check_exist:
+        N_f.append(d[index])
+        Check_exist.add(index)
+        index = d[index]
+        print(index)
     else:
-        M_f.append(L[Middle_index])
-        if len(L)%2 ==0:
-            for index in range(Middle_index):
-                M_f.append(L[index])
-                M_f.append(L[-(index+1)])
-            M_f.pop()
+        for i in range(len(L)):
+            if(i) not in Check_exist:
+                N_f.append(d[i])
+                Check_exist.add(i)
+                index = d[i]
+                print(index)
+                break
+        if d[index] not in Check_exist:
+            N_f.append(d[index])
+            Check_exist.add(index)
+            index = d[index]
+            print(index)
         else:
-             for index in range(Middle_index):
-                M_f.append(L[index])
-                M_f.append(L[-(index+1)])
-    return M_f
-
+            for i in range(len(L)):
+                if(i) not in Check_exist:
+                    N_f.append(d[i])
+                    Check_exist.add(i)
+                    index = d[i]
+                    print(index)
+                    break    
         
-M=push_middle(L)        
+  
+     
+  
 
-print('\nHere is M:')
-print('  ', M)
-print('\nHere is N:')
-print('  ', N)
-print('\nHere is L again:')
-print('  ', L)
+print(N_f)
+print(Check_exist)
